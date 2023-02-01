e7ee9853ac8bf1e36e4dd5aa566a95d29b92f5aa
Dylan <kaosanct@naver.com>
2023-02-01 23:03:29 +0700

    react basic

---
+++ template/src/AppProducts.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Products from "./components/Products";
+import { useState } from "react";
+export default function AppProducts() {
+  const [showProducts, setShowProducts] = useState(true);
+  return (
+    <div>
+      {showProducts && <Products />}
+      <button onClick={() => setShowProducts((show) => !show)}>Toggle</button>
+    </div>
+  );
+}
--- template/src/components/Counter.jsx
+++ template/src/components/Counter.jsx
@@ -1,22 +1,22 @@
 import React, { useState } from "react";
 
-export default function Counter() {
+export default function Counter({ total, onClick }) {
   const [count, setCount] = useState(0);
   const onIncrease = () => {
-    setCount(count + 1);
-  };
-  const onDecrease = () => {
-    if (count <= 0) return;
-    setCount(count - 1);
+    setCount((prev) => prev + 1);
   };
   return (
     <div className="counter">
-      <span className="number">{count}</span>
-      <button className="button" onClick={onIncrease}>
+      <span className="number">{count}</span>{" "}
+      <span className="total">/{total}</span>
+      <button
+        className="button"
+        onClick={() => {
+          onIncrease();
+          onClick();
+        }}
+      >
         Add +
-      </button>
-      <button className="button" onClick={onDecrease}>
-        Minus -
       </button>
     </div>
   );
